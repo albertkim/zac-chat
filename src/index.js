a77688bf0d1080294fc916140ba15ece24ca5a7c
Albert Kim <albert275@gmail.com>
2019-01-01 19:15:15 -0800

    Added express body parser

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 require('dotenv').config()
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = process.env.PORT
 
 const chats = []
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => res.send('Zac chat!'))
 
 app.get('/chats', (req, res, next) => {
   res.send(chats)
